4b27277514df2c575e2136a645532a28e4eec21d
A. M Samdani Mozumder <samdanimozumder91@gmail.com>
2024-11-09 13:13:42 +0600

    Getting started with landing page and authentication

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,41 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require("cors")
+const UserModel = require('./db/user')
+
 const app = express();
-const PORT = process.env.PORT || 5000;
+app.use(express.json())
+app.use(cors())
+
+const PORT = 3001;
+
+mongoose.connect("mongodb://127.0.0.1:27017/SPL2");
+
+app.post('/register',(req,res) => {
+    UserModel.create(req.body)
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
+app.post('/login',(req,res) => {
+    const {email,password} = req.body;
+    UserModel.findOne({email:email})
+    .then(user => {
+        if(user){
+            if(user.password === password){
+                res.json("Success");
+            }else{
+                res.json("Incorrect Password");
+            }
+        }else{
+            res.json("User doesn't exist");
+        }
+    })
+})
 
 app.get('/', (req, res) => {
-    res.send('Backend Server Running');
+    res.send('Backend Server Running')
+    
 });
 
 app.listen(PORT, () => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Signup from './Components/Authentication/Signup';
+import Login from './Components/Authentication/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Landing from './Components/Landing';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+			<Route path='/' element={<Landing/>}></Route>
+				<Route path='/register' element={<Signup/>}></Route>
+				<Route path='/login' element={<Login/>}>
+				</Route>
+			</Routes>
+
+		</BrowserRouter>
+	);
 }
 
 export default App;
