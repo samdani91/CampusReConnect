33fb5b4815f0259d5c3c080c34871f25f814ddd0
A. M Samdani Mozumder <samdanimozumder91@gmail.com>
2024-12-12 19:08:43 +0600

    Update Landing.js

--- frontend/src/Components/Home/Landing.js
+++ frontend/src/Components/Home/Landing.js
@@ -13,7 +13,7 @@ export default function Landing() {
                     <button className="navbar-toggler " type="button" data-bs-toggle="collapse"
                         data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                         aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                        <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
