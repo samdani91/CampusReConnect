414ee58e97a133ebb860919e97a7675da7ce6662
A. M Samdani Mozumder <samdanimozumder91@gmail.com>
2024-12-15 18:46:40 +0600

    Create Notification.js

---
+++ frontend/src/Components/Notification.js
@@ -0,0 +1,30 @@
+import React, { useContext } from "react";
+import { NotificationContext } from "./Context/NotificationContext";
+import Navbar from './Navbar/Navbar'
+
+const Notification = () => {
+    const { notifications } = useContext(NotificationContext);
+
+    return (
+        <>
+            <Navbar />
+            <div className="container mt-4">
+                <h1 className="mb-4">Notifications</h1>
+
+                {notifications.length > 0 ? (
+                    <ul className="list-group">
+                        {notifications.map((notification) => (
+                            <li key={notification.id} className="list-group-item">
+                                {notification.message}
+                            </li>
+                        ))}
+                    </ul>
+                ) : (
+                    <p>You have no new notifications.</p>
+                )}
+            </div>
+        </>
+    );
+};
+
+export default Notification;
