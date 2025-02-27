aedce694533373f455afbdfefedf77fef5b4398b
A. M Samdani Mozumder <samdanimozumder91@gmail.com>
2025-02-28 01:00:19 +0600

    Create post.sql

---
+++ sql_scripts/post.sql
@@ -0,0 +1,17 @@
+CREATE TABLE spl2.post (
+    post_id VARCHAR(100) PRIMARY KEY,
+    user_id VARCHAR(30),
+    post_type VARCHAR(30),
+    topic VARCHAR(100),
+    title VARCHAR(400),
+    authors VARCHAR(100),
+    description VARCHAR(10000),
+    attachment VARCHAR(200),
+    summary VARCHAR(2000),
+    visibility BIT(1),
+    upvotes INT(5),
+    downvotes INT(5),
+    created_date VARCHAR(50),
+    community_id VARCHAR(30)
+    -- FOREIGN KEY (user_id) REFERENCES spl2.user(user_id)  -- Assuming a Users table exists
+);
