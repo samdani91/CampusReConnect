83a18c1ae9a880a5b07d709407b4b1dda6f1a0d4
A. M Samdani Mozumder <samdanimozumder91@gmail.com>
2025-03-06 03:33:10 +0600

    Update Post.js
    3 dot UI updatted

--- frontend/src/Components/Post/Post.js
+++ frontend/src/Components/Post/Post.js
@@ -343,7 +343,7 @@ const Post = ({ postId, postOwnerId, title, topic, description, authors, pdfUrl,
     return (
         <div className="card mb-2">
             <div className="card-body text-start">
-                <div className="d-flex justify-content-between align-items-center mb-3">
+                <div className="d-flex justify-content-between mb-3">
                     <h2 className="card-title mb-0">{title}</h2>
                     <div className="dropdown">
                         <button
